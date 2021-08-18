326bd8b61866d877c7fd8327ab367a7fe3c9114e
Guilherme <85498898+GuilhermeA2021@users.noreply.github.com>
2021-08-18 17:31:07 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+/*const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';*/
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/moto.jpg') {
+      myImage.setAttribute('src','images/index3.jpg');
+    } else {
+      myImage.setAttribute('src','images/moto.jpg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
